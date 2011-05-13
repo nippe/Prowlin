2798c5a7cd680e0c6072f3b74ad78647d41955a0
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-05-13 17:46:16 +0200

    issed two riles

---
+++ Prowlin/Notification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prowlin
+{
+    public class Notification
+    {
+        private string _url = string.Empty;
+
+        public NotificationPriority Priority {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public string Url {
+            get { return _url; }
+            set {
+                if(value.Length > 512)
+                {
+                    throw new ArgumentException("Url can maximum be 512 characters");
+                }
+                _url = value;
+            }
+        }
+
+        
+        public string Event {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+
+        public string Description {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Prowlin/NotificationPriority.cs
@@ -0,0 +1,11 @@
+﻿namespace Prowlin
+{
+    public enum NotificationPriority
+    {
+        VeryLow = -2,
+        Moderate = -1,
+        Normal = 0,
+        High = 1,
+        Emergency = 2
+    }
+}
