9f6077074d022ec6b10cbad2ad69a0c3da87e857
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-05-13 17:45:53 +0200

    Before going for data annotaions

--- Prowlin.UnitTests/NotificationTests.cs
+++ Prowlin.UnitTests/NotificationTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 
+
 namespace Prowlin.UnitTests
 {
     
@@ -15,6 +16,33 @@ namespace Prowlin.UnitTests
         {
             Assert.True(true);
         }
+
+        [Fact]
+        public void assigning_values_to_properties_should_wrok() {
+            
+            Notification notification = new Notification();
+            
+            notification.Priority = NotificationPriority.Normal;
+            notification.Url = "http://nnihlen.com/blog";
+            notification.Event = "Test event";
+            notification.Description = "Description of test event...";
+        }
+
+        //[Fact]
+        //public void assigning_Url_more_than_512_characters_should_throw_exception() {
+        //    string s = new string('n', 600);
+
+        //    Notification notification = new Notification();
+
+        //    Assert.Throws<ArgumentException>(
+        //        delegate
+        //            {
+        //                notification.Url = s;
+        //            });
+        //}
+
+      
+            
 
     }
 }
