7e3e7b6ceb1622a96ac810501b4adf70cf13277c
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-05-13 15:03:31 +0200

    Initial commit

---
+++ Prowlin.UnitTests/NotificationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Prowlin.UnitTests
+{
+    
+    public class NotificationTests
+    {
+
+        [Fact]
+        public void this_should_work()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
